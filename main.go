520faa867b1f361bf4bdea7ffd13a7fb30a42761
Ryota <rytswd@gmail.com>
2020-04-07 00:46:17 +0100

    Add local proto dependency, set gazelle:proto disable_global, for testing

--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 
 	_ "cloud.google.com/go/language/apiv1"
 	"cloud.google.com/go/spanner"
+
+	"github.com/rytswd/simple-bazel/timesvc"
 )
 
 func main() {
 	ctx := context.Background()
 	_ = spanner.Statement{}
+	_ = timesvc.GetNowResponse{}
 	_ = ctx
 	fmt.Println("hello")
 }
