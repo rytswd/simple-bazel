d770a85d222720517c1799932494cbbcbdadc1f1
Ryota <rytswd@gmail.com>
2020-11-22 03:17:26 +0000

    Add comment

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 	_ "cloud.google.com/go/language/apiv1"
 	"cloud.google.com/go/spanner"
+
+	// Causes Bazel link error
 	_ "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 
 	"github.com/rytswd/simple-bazel/timesvc"
